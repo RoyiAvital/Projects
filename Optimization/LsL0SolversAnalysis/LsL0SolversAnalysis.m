fd460b8461113de416e9d33e69cbebe7014ec21b
Royi <RoyiAvital@yahoo.com>
2017-12-10 07:16:48 +0200

    New 	- 	Added L0 Regularized LS. Fixed 	- 	B. Known Issues 	-	Image Convolution project's DLL produced by VS 2015 in Release Mode (OpenMP) crashes MATLAB (GCC does not, yet seems slower). The Executable works. To Do 	- 	Recreate the C code of Levinson Recursion project. 	- 	Generalize the Interior Point Solver. 	- 	Better documentation for the ImageConvolution project.

---
+++ Optimization/LsL0SolversAnalysis/LsL0SolversAnalysis.m
@@ -0,0 +1,283 @@
+% Least Squares Regularized by L0 Norm - Solvers Analysis
+% Solving: \arg \min_{x} 0.5 * || A x - b ||_{2}^{2} + \lambda || x ||_{0}
+% References:
+%   1.  aa
+% Remarks:
+%   1.  
+% Known Issues:
+%   1.  IRLS Solver doesn't work.
+% TODO:
+% 	1.  Add Coordinate Descent based algorithm.
+% Release Notes
+% - 1.0.000     08/12/2017
+%   *   First release.
+
+
+%% General Parameters
+
+run('InitScript.m');
+
+figureIdx           = 0; %<! Continue from Question 1
+figureCounterSpec   = '%04d';
+
+generateFigures = OFF;
+
+
+%% Simulation Parameters
+
+numRows = 6;
+numCols = 25; %<! Number of Vectors - i (K in the question)
+
+paramLambda = 0.1;
+tolVal      = 1e-6;
+
+numIterations   = 500;
+stopThr         = 0;
+
+solverIdx       = 0;
+cLegendString   = {};
+
+mObjFunVal  = zeros([numIterations, 1]);
+mSolErrNorm = zeros([numIterations, 1]);
+
+
+%% Generate Data
+
+mA = randn([numRows, numCols]);
+vB = randn([numRows, 1]);
+
+hL0Norm = @(vX) sum(abs(vX) > tolVal);
+hObjFun = @(vX) (0.5 * sum(((mA * vX) - vB) .^ 2)) + (paramLambda * hL0Norm(vX));
+
+
+%% Solution by Matching Pursuit
+
+[vX, mX] = SolveLsL0Mp(mA, vB, paramLambda, numIterations, tolVal);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['MP Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument L0 Norm - ', num2str(hL0Norm(vX))]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+vIdx = find(vX);
+
+
+%% Solution by Orthogonal Matching Pursuit
+
+[vX, mX] = SolveLsL0Omp(mA, vB, paramLambda, numIterations, tolVal);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['OMP Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument L0 Norm - ', num2str(hL0Norm(vX))]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+
+%% Solution by Sub Gradient Method
+
+[vX, mX] = SolveLsL1SubGradient(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Sub Gradient Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument L0 Norm - [ ', num2str(vX.'), ' ]']);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Sub Gradient'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by Smoothing (Huber Loss) Method
+
+[vX, mX] = SolveLsL1Huber(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Smoothing (Huber Loss) Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Smoothing (Huber Loss)'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by Proximal Gradient Method
+
+[vX, mX] = SolveLsL1Prox(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Proximal Gradient Method Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Proximal Gradient'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by Proximal Gradient Method with Line Search
+
+[vX, mX] = SolveLsL1ProxLs(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Proximal Gradient Method (LS) Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Proximal Gradient - Line Search'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by Proximal Gradient Method Accelerated
+
+[vX, mX] = SolveLsL1ProxAccel(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Proximal Gradient Method (Accelerated) Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Proximal Gradient - Accelerated'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by Proximal Gradient Method Accelerated with Line Search
+
+[vX, mX] = SolveLsL1ProxAccelLs(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['Proximal Gradient Method (Accelerated + LS) Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['Proximal Gradient - Accelerated + Line Search'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by ADMM
+
+[vX, mX] = SolveLsL1Admm(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['ADMM Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['ADMM'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by IRLS
+
+[vX, mX] = SolveLsL1Irls(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['IRLS Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['IRLS'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Solution by CD
+
+[vX, mX] = SolveLsL1Cd(mA, vB, paramLambda, numIterations);
+
+objVal = hObjFun(vX);
+
+disp([' ']);
+disp(['CD Solution Summary']);
+disp(['The Optimal Value Is Given By - ', num2str(objVal)]);
+disp(['The Optimal Argument Is Given By - [ ', num2str(vX.'), ' ]']);
+disp([' ']);
+
+solverIdx                   = solverIdx + 1;
+cLegendString{solverIdx}    = ['CD'];
+
+[mObjFunVal, mSolErrNorm] = UpdateAnalysisData(mObjFunVal, mSolErrNorm, mX, hObjFun, sCvxSol, solverIdx);
+
+
+%% Display Results
+
+hFigure     = figure('Position', figPosLarge);
+
+hAxes       = subplot_tight(2, 1, 1, [0.09, 0.09]);
+hLineSeries = plot(1:numIterations, 10 * log10(mObjFunVal));
+set(hLineSeries, 'LineWidth', lineWidthNormal);
+set(get(hAxes, 'Title'), 'String', ['Objective Function Value vs. Optimal Value (CVX)'], ...
+    'FontSize', fontSizeTitle);
+set(get(hAxes, 'XLabel'), 'String', 'Iteration Number', ...
+    'FontSize', fontSizeAxis);
+set(get(hAxes, 'YLabel'), 'String', 'Objective Function Value', ...
+    'FontSize', fontSizeAxis);
+set(hAxes, 'XLim', [1, numIterations]);
+hLegend = ClickableLegend(cLegendString);
+
+hAxes       = subplot_tight(2, 1, 2, [0.09, 0.09]);
+hLineSeries = plot(1:numIterations, 10 * log10(mSolErrNorm));
+set(hLineSeries, 'LineWidth', lineWidthNormal);
+set(get(hAxes, 'Title'), 'String', ['Solution Error Norm'], ...
+    'FontSize', fontSizeTitle);
+set(get(hAxes, 'XLabel'), 'String', 'Iteration Number', ...
+    'FontSize', fontSizeAxis);
+set(get(hAxes, 'YLabel'), 'String', 'Objective Function Value', ...
+    'FontSize', fontSizeAxis);
+set(hAxes, 'XLim', [1, numIterations]);
+hLegend = ClickableLegend(cLegendString);
+
+if(generateFigures == ON)
+    saveas(hFigure,['Figure', num2str(figureIdx, figureCounterSpec), '.png']);
+end
+
+
+%% Restore Defaults
+
+% set(0, 'DefaultFigureWindowStyle', 'normal');
+% set(0, 'DefaultAxesLooseInset', defaultLoosInset);
+
---
+++ Optimization/LsL0SolversAnalysis/SolveLsL0Omp.m
@@ -0,0 +1,81 @@
+function [ vX, mX ] = SolveLsL0Omp( mA, vB, paramLambda, numIterations, tolVal )
+% ----------------------------------------------------------------------------------------------- %
+%[ vX, mX ] = SolveLsL0Mp( mA, vB, paramLambda, numIterations )
+% Solve L0 Regularized Least Squares Using Matching Pursuit (MP) Method.
+% Input:
+%   - mA                -   Input Matirx.
+%                           The model matrix.
+%                           Structure: Matrix (m X n).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - vB                -   input Vector.
+%                           The model known data.
+%                           Structure: Vector (m X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - paramLambda       -   Parameter Lambda.
+%                           The L1 Regularization parameter.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: (0, inf).
+%   - numIterations     -   Number of Iterations.
+%                           Number of iterations of the algorithm.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range {1, 2, ...}.
+% Output:
+%   - vX                -   Output Vector.
+%                           Structure: Vector (n X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+% References
+%   1.  Wikipedia MP - https://en.wikipedia.org/wiki/Matching_pursuit.
+% Remarks:
+%   1.  U.
+% Known Issues:
+%   1.  A
+% TODO:
+%   1.  Pre Process 'mA' by normalizing its columns.
+% Release Notes:
+%   -   1.0.000     04/12/2017
+%       *   First realease version.
+% ----------------------------------------------------------------------------------------------- %
+
+numRows = size(mA, 1);
+numCols = size(mA, 2);
+
+% Since all atoms are used and the solutions is optimal, number of
+% iterations can not exceed number of atoms as the resdiaul is at its
+% minimum.
+numIterations = min(numIterations, numCols);
+
+vActiveIdx  = false([numCols, 1]);
+vR          = vB;
+vX          = zeros([numCols, 1]);
+vCostFun    = zeros([numIterations, 1]);
+vResNorm    = zeros([numIterations, 1]);
+mX          = zeros([numCols, numIterations]);
+
+mAA = mA ./ sum(mA .* mA);
+
+for ii = 1:numIterations
+    
+    vResNorm(ii) = norm(vR);
+    
+    [~, activeIdx] = max(abs(mAA.' * vR));
+    vActiveIdx(activeIdx) = true([1, 1]);
+    
+    vX(vActiveIdx) = mA(:, vActiveIdx) \ vB;
+    vR = vR - (mA(:, vActiveIdx) * vX(vActiveIdx));
+    
+    mX(:, ii) = vX;
+    vCostFun(ii) = (0.5 * sum(((mA * vX) - vB) .^ 2)) + (paramLambda * sum(abs(vX) > tolVal));
+    
+end
+
+[~, minCostValIdx] = min(vCostFun);
+vX = mX(:, minCostValIdx);
+
+
+end
+
