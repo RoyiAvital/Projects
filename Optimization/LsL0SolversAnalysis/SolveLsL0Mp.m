8eac340f5ff28b003add893f194e1cd8d8fe2a2d
Royi <RoyiAvital@yahoo.com>
2017-12-10 07:16:48 +0200

    New 	- 	Added L0 Regularized LS. Fixed 	- 	B. Known Issues 	-	Image Convolution project's DLL produced by VS 2015 in Release Mode (OpenMP) crashes MATLAB (GCC does not, yet seems slower). The Executable works. To Do 	- 	Recreate the C code of Levinson Recursion project. 	- 	Generalize the Interior Point Solver. 	- 	Better documentation for the ImageConvolution project.

---
+++ Optimization/LsL0SolversAnalysis/SolveLsL0Mp.m
@@ -0,0 +1,74 @@
+function [ vX, mX ] = SolveLsL0Mp( mA, vB, paramLambda, numIterations, tolVal )
+% ----------------------------------------------------------------------------------------------- %
+%[ vX, mX ] = SolveLsL0Mp( mA, vB, paramLambda, numIterations )
+% Solve L0 Regularized Least Squares Using Matching Pursuit (MP) Method.
+% Input:
+%   - mA                -   Input Matirx.
+%                           The model matrix.
+%                           Structure: Matrix (m X n).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - vB                -   input Vector.
+%                           The model known data.
+%                           Structure: Vector (m X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+%   - paramLambda       -   Parameter Lambda.
+%                           The L1 Regularization parameter.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range: (0, inf).
+%   - numIterations     -   Number of Iterations.
+%                           Number of iterations of the algorithm.
+%                           Structure: Scalar.
+%                           Type: 'Single' / 'Double'.
+%                           Range {1, 2, ...}.
+% Output:
+%   - vX                -   Output Vector.
+%                           Structure: Vector (n X 1).
+%                           Type: 'Single' / 'Double'.
+%                           Range: (-inf, inf).
+% References
+%   1.  Wikipedia MP - https://en.wikipedia.org/wiki/Matching_pursuit.
+% Remarks:
+%   1.  U.
+% Known Issues:
+%   1.  A
+% TODO:
+%   1.  Pre Process 'mA' by normalizing its columns.
+% Release Notes:
+%   -   1.0.000     04/12/2017
+%       *   First realease version.
+% ----------------------------------------------------------------------------------------------- %
+
+numRows = size(mA, 1);
+numCols = size(mA, 2);
+
+vActiveIdx  = false([numCols, 1]);
+vR          = vB;
+vX          = zeros([numCols, 1]);
+vCostFun    = zeros([numIterations, 1]);
+vResNorm    = zeros([numIterations, 1]);
+mX          = zeros([numCols, numIterations]);
+
+for ii = 1:numIterations
+    
+    vResNorm(ii) = norm(vR);
+    
+    [~, activeIdx] = max(abs(mA.' * vR));
+    vActiveIdx(activeIdx) = true([1, 1]);
+    
+    vX(activeIdx) = (mA(:, activeIdx).' * vR) ./ (mA(:, activeIdx).' * mA(:, activeIdx));
+    vR = vR - (mA(:, activeIdx) * vX(activeIdx));
+    
+    mX(:, ii) = vX;
+    vCostFun(ii) = (0.5 * sum(((mA * vX) - vB) .^ 2)) + (paramLambda * sum(abs(vX) > tolVal));
+    
+end
+
+[~, minCostValIdx] = min(vCostFun);
+vX = mX(:, minCostValIdx);
+
+
+end
+
