71dbe5e4f07268422b576faacb74fe1976b3dfcf
Royi <RoyiAvital@yahoo.com>
2018-04-20 04:44:44 +0300

    New 	- 	Added Calculate Distance Matrix project. Fixed 	- 	TB Known Issues 	-	Image Convolution project's DLL produced by VS 2015 in Release Mode (OpenMP) crashes MATLAB (GCC does not, yet seems slower). The Executable works. To Do 	- 	Recreate the C code of Levinson Recursion project. 	- 	Generalize the Interior Point Solver. 	- 	Better documentation for the ImageConvolution project. 	- 	Update the projects to VS 2017. Remarks 	- 	E

--- CalcDistanceMatrix/CalcDistanceMatrixUnitTest001.m
+++ CalcDistanceMatrix/CalcDistanceMatrixUnitTest001.m
@@ -1,13 +1,14 @@
 % ----------------------------------------------------------------------------------------------- %
-% Calc Distanc Matrix Unit Test 001
+% Calculate Dsitance Matrix Unit Test 009 - 'CalcDistanceMatrix()'
 % Reference:
-%   1. Checks only the Iterative K-Means.
+%   1. fd
 % Remarks:
-%   1.  Working within the [0, 1] range in RGB.
+%   1.  Working on Float (Single).
+%   2.  M
 % TODO:
 %   1.  A
 %   Release Notes:
-%   -   1.0.000     06/02/2017  Royi Avital
+%   -   1.0.000     19/04/2018  Royi Avital
 %       *   First release version.
 % ----------------------------------------------------------------------------------------------- %
 
@@ -15,65 +16,97 @@
 
 run('InitScript.m');
 
-% if(libisloaded('ClusterImageColorsAuxSseDll') == FALSE)
-%     % loadlibrary('x64\Release\ClusterImageColorsAuxSseDll.dll', 'ClusterImageColorsAuxSse\ClusterImageColorsAuxSseDll.h');
-%     loadlibrary('x64\Debug\ClusterImageColorsAuxSseDll.dll', 'ClusterImageColorsAuxSse\ClusterImageColorsAuxSseDll.h');
-%     libfunctions('ClusterImageColorsAuxSseDll', '-full')
-% end
+COMPILING_MODE_DEBUG    = 1;
+COMPILING_MODE_RELEASE  = 2;
+COMPILING_MODE_GCC      = 3;
+COMPILING_MODE_ICC      = 4;
+
+LIB_NAME            = 'CalcDistanceMatrixDll';
+H_FILE_NAME         = 'CalcDistanceMatrixDll';
+LIB_PATH_DEBUG      = 'x64\Debug\';
+LIB_PATH_RELEASE    = 'x64\Release\';
+LIB_PATH_GCC        = 'x64\GCC\';
+LIB_PATH_ICC        = 'x64\ICC\'; %<! Intel Compiler
+H_FILE_PATH         = 'CalcDistanceMatrix\';
 
 
-%% Setting Parameters
+%% Settings
 
-% Data Parameters
-numFeatures     = 10;
-numSamples001   = 10000;
-numSamples002   = 25;
-
-numIterations = 50;
+compilingMode   = COMPILING_MODE_GCC;
 
 
-%% Generating Data
+%% Loading Library
 
-mDataSamples001 = randn([numFeatures, numSamples001], 'single');
-mDataSamples002 = randn([numFeatures, numSamples002], 'single');
-
-
-%% Running Algorithms
-
-% Method 001
-hRunTimer = tic();
-for ii = 1:numIterations
-    mDistMat001 = CalcDistanceMatrix001(mDataSamples001, mDataSamples002);
+switch(compilingMode)
+    case(COMPILING_MODE_DEBUG)
+        libFullPath = [LIB_PATH_DEBUG, LIB_NAME, '.dll'];
+    case(COMPILING_MODE_RELEASE)
+        libFullPath = [LIB_PATH_RELEASE, LIB_NAME, '.dll'];
+    case(COMPILING_MODE_GCC)
+        libFullPath = [LIB_PATH_GCC, LIB_NAME, '.dll'];
+    case(COMPILING_MODE_ICC)
+        libFullPath = [LIB_PATH_ICC, LIB_NAME, '.dll'];
 end
-runTime = toc(hRunTimer);
 
-disp(['Method 001 Run Time - ', num2str(runTime), ' [Sec]']);
+headerFullPath = [H_FILE_PATH, H_FILE_NAME, '.h'];
+
+if(libisloaded(LIB_NAME) == FALSE)
+    loadlibrary(libFullPath, headerFullPath);
+    libfunctions(LIB_NAME, '-full');
+end
+
+
+%% Analysis 'ConvertFromUint8Fa()'
+
+% funName         = 'CalcDistanceMatrixVanilla';
+% funName         = 'CalcDistanceMatrixSse';
+% funName         = 'CalcDistanceMatrixAvx';
+funName         = 'CalcDistanceMatrixEigen';
+
+vecDim      = 80;
+numColsA    = 8000;
+numColsB    = 6000;
+
+mA = randn([vecDim, numColsA], 'single');
+mB = randn([vecDim, numColsB], 'single');
+mD = zeros([numColsA, numColsB], 'single');
+
+hRunTime = tic();
+% mDRef = CalcDistanceMatrix001(mA, mB); %<! Slower
+mDRef = CalcDistanceMatrix002(mA, mB); %<! Faster
+matlabRunTime = toc(hRunTime);
+
+vecDim = int32(vecDim);
+numColsA = int32(numColsA);
+numColsB = int32(numColsB);
+
+hRunTime = tic();
+mD = calllib(LIB_NAME, funName, mD, mA, mB, vecDim, numColsA, numColsB);
+dllRunTime = toc(hRunTime);
+
+vE = mD(:) - mDRef(:);
+maxAbsErr = max(abs(vE));
+rmseErr = sqrt(mean(vE .* vE));
+
+disp([' ']);
+disp([funName, ' Unit Test']);
+disp(['Max Abs Error - ', num2str(maxAbsErr)]);
+disp(['RMSE Error - ', num2str(rmseErr)]);
+disp(['MATLAB Run Time - ', num2str(matlabRunTime)]);
+disp(['DLL Run Time - ', num2str(dllRunTime)]);
 disp([' ']);
 
-% Method 002
-hRunTimer = tic();
-for ii = 1:numIterations
-    mDistMat002 = CalcDistanceMatrix002(mDataSamples001, mDataSamples002);
+
+%% Restore Defaults
+
+if(libisloaded(LIB_NAME) == TRUE)
+    unloadlibrary(LIB_NAME);
 end
-runTime = toc(hRunTimer);
 
-disp(['Method 002 Run Time - ', num2str(runTime), ' [Sec]']);
-disp([' ']);
-
-
-%% Displaying Results
-
-
-%% Restoring Defaults
-
-% if(libisloaded('ClusterImageColorsAuxSseDll') == TRUE)
-%     unloadlibrary('ClusterImageColorsAuxSseDll');
-% end
-% 
-% if(libisloaded('ClusterImageColorsAuxSseDll') == TRUE)
-%     disp('Error UnLoading ClusterImageColorsAuxSseDll Library');
-% end
+if(libisloaded(LIB_NAME) == TRUE)
+    disp('Error UnLoading ', LIB_NAME, ' Library');
+end
 
 % set(0, 'DefaultFigureWindowStyle', 'normal');
-% set(0, 'DefaultAxesLooseInset', defaultLooseInset);
+% set(0, 'DefaultAxesLooseInset', defaultLoosInset);
 
