f564f6cd0d571e41ba5f6237fbb807def3eb3b14
Royi <RoyiAvital@yahoo.com>
2018-04-20 15:26:21 +0300

    New 	- 	Added Calculate Distance Matrix project. Fixed 	- 	TB Known Issues 	-	Image Convolution project's DLL produced by VS 2015 in Release Mode (OpenMP) crashes MATLAB (GCC does not, yet seems slower). The Executable works. To Do 	- 	Recreate the C code of Levinson Recursion project. 	- 	Generalize the Interior Point Solver. 	- 	Better documentation for the ImageConvolution project. 	- 	Update the projects to VS 2017. Remarks 	- 	E

--- CalcDistanceMatrix/CalcDistanceMatrix/CalcDistanceMatrixMain.h
+++ CalcDistanceMatrix/CalcDistanceMatrix/CalcDistanceMatrixMain.h
@@ -9,6 +9,7 @@
 #define UNIT_TEST_CALC_DISTANCE_MATRIX_SSE			2
 #define UNIT_TEST_CALC_DISTANCE_MATRIX_AVX			3
 #define UNIT_TEST_CALC_DISTANCE_MATRIX_EIGEN		4
+#define UNIT_TEST_CALC_DISTANCE_MATRIX_EIGEN_M		5
 
 
 // FA Library Functions
@@ -16,6 +17,7 @@ void CalcDistanceMatrixVanilla(float* mD, float* mA, float* mB, int vecDim, int 
 void CalcDistanceMatrixSse(float* mD, float* mA, float* mB, int vecDim, int numRowsA, int numRowsB);
 void CalcDistanceMatrixAvx(float* mD, float* mA, float* mB, int vecDim, int numRowsA, int numRowsB);
 void CalcDistanceMatrixEigen(float* mD, float* mA, float* mB, int vecDim, int numRowsA, int numRowsB);
+void CalcDistanceMatrixEigenM(float* mD, float* mA, float* mB, int vecDim, int numRowsA, int numRowsB);
 void CalcDistanceMatrixRefTime(float* mD, float* mA, float* mB, int vecDim, int numRowsA, int numRowsB);
 
 // Unit Test Functions 
@@ -23,6 +25,7 @@ void CalcDistanceMatrixVanillaUnitTest(int vecDim, int numRowsA, int numRowsB, i
 void CalcDistanceMatrixSseUnitTest(int vecDim, int numRowsA, int numRowsB, int numIter);
 void CalcDistanceMatrixAvxUnitTest(int vecDim, int numRowsA, int numRowsB, int numIter);
 void CalcDistanceMatrixEigenUnitTest(int vecDim, int numRowsA, int numRowsB, int numIter);
+void CalcDistanceMatrixEigenMUnitTest(int vecDim, int numRowsA, int numRowsB, int numIter);
 
 // Auxiliary Functions
 void PrintRunTimeData(double *vRunTime, int numIter);
