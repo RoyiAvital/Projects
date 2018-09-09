03a85b5459591106bc7c1c8ac5c10e3997155f97
Royi <RoyiAvital@yahoo.com>
2018-09-09 14:23:29 +0300

    New 	- 	A Fixed 	- 	Code spacing in Unit Tests. Known Issues 	-	Image Convolution project's DLL produced by VS 2015 in Release Mode (OpenMP) crashes MATLAB (GCC does not, yet seems slower). The Executable works. To Do 	- 	Recreate the C code of Levinson Recursion project. 	- 	Generalize the Interior Point Solver. 	- 	Better documentation for the ImageConvolution project. 	- 	Update the projects to VS 2017. Remarks 	- 	E

--- ImageBilateralFilter/ImageBilateralFilterUnitTest003.m
+++ ImageBilateralFilter/ImageBilateralFilterUnitTest003.m
@@ -18,6 +18,7 @@ run('InitScript.m');
 COMPILING_MODE_DEBUG    = 1;
 COMPILING_MODE_RELEASE  = 2;
 COMPILING_MODE_GCC      = 3;
+COMPILING_MODE_ICC      = 4;
 
 DYNAMIC_LIB_POSTFIX_WIN     = '.dll';
 DYNAMIC_LIB_POSTFIX_LINUX   = '.so';
@@ -28,13 +29,14 @@ H_FILE_NAME         = 'ImageBilateralFilterDll';
 LIB_PATH_DEBUG      = 'x64/Debug/';
 LIB_PATH_RELEASE    = 'x64/Release/';
 LIB_PATH_GCC        = 'x64/GCC/';
+LIB_PATH_ICC        = 'x64/ICC/';
 H_FILE_PATH         = 'ImageBilateralFilter/';
 
 
 %% Settings
 
 funName         = 'ImageConvolutionGaussianKernel';
-compilingMode   = COMPILING_MODE_GCC;
+compilingMode   = COMPILING_MODE_ICC;
 
 if(ispc())
     dyLibPostfix = DYNAMIC_LIB_POSTFIX_WIN;
@@ -55,7 +57,6 @@ stdToRadiusFactor   = 5;
 
 %% Loading Library
 
-
 switch(compilingMode)
     case(COMPILING_MODE_DEBUG)
         libFullPath = [LIB_PATH_DEBUG, LIB_NAME, dyLibPostfix];
@@ -63,6 +64,8 @@ switch(compilingMode)
         libFullPath = [LIB_PATH_RELEASE, LIB_NAME, dyLibPostfix];
     case(COMPILING_MODE_GCC)
         libFullPath = [LIB_PATH_GCC, LIB_NAME, dyLibPostfix];
+    case(COMPILING_MODE_ICC)
+        libFullPath = [LIB_PATH_ICC, LIB_NAME, dyLibPostfix];
 end
 
 headerFullPath = [H_FILE_PATH, H_FILE_NAME, '.h'];
